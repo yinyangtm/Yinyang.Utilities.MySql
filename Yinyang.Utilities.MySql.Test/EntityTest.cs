3919d551dbc56a3372ee40eb6942ce1e56eede73
Nobuo Miura <84451944+nobuo-miura@users.noreply.github.com>
2023-07-12 12:05:41 +0900

    Version 1.0.0

---
+++ Yinyang.Utilities.MySql.Test/EntityTest.cs
@@ -0,0 +1,9 @@
+namespace Yinyang.Utilities.MySql.Test
+{
+    internal class EntityTest
+    {
+        public int id { get; set; }
+        public int key { get; set; }
+        public string value { get; set; }
+    }
+}
